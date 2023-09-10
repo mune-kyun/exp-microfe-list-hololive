fbf7edd67a1580b47f4addee285ac115f8154d43
mune-kyun <rzknurfaizi@gmail.com>
2023-09-10 11:48:11 +0700

    card and its event listener

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Card from "./components/Card.svelte";
+  
+  let data = [
+    {
+      name: "Sakura Miko",
+      key: "miko",
+      bgImg: "https://media.tenor.com/p86jSzfI-KEAAAAC/sakura-miko.gif"
+    },
+    {
+      name: "Minato Aqua",
+      key: "aqua",
+      bgImg: "https://media.tenor.com/SAg8wyuG9N4AAAAd/minato-aqua-minato.gif"
+    },
+    {
+      name: "Tokoyami Towa",
+      key: "towa",
+      bgImg: "https://media.tenor.com/ztlNAqw_00EAAAAC/%E5%B8%B8%E9%97%87%E3%83%88%E3%83%AF-%E3%83%9B%E3%83%AD%E3%83%A9%E3%82%A4%E3%83%96.gif"
+    }
+  ];
+
+  const handleCardClick = (name) => {
+    const event = new CustomEvent("InterComponent", {
+      detail: {
+        customData: {
+          from: "list-hololive",
+          action: "click-card",
+          additionalData: {
+            name
+          }
+        },
+      },
+    });
+    dispatchEvent(event);
+  }
+</script>
+
+<style>
+  * {
+    font-family: 'Quicksand', sans-serif;
+  }
+</style>
+
+<section class="w-full min-h-screen flex flex-col justify-center items-center gap-6 p-4">
+<h1 class="text-5xl text-center font-semibold text-[#50e0ff]">TALENTS</h1>
+<div class="w-full flex justify-center items-center gap-10">
+  {#each data as {name, key, bgImg}}
+  <div on:click={() => handleCardClick(key)}>
+    <Card name={name} bgImg={bgImg} />
+  </div>
+  {/each}
+</div>
+</section>
---
+++ src/components/Card.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let name, bgImg;
+</script>
+
+<div class="p-3 w-[200px] h-[300px] rounded-xl overflow-hidden flex justify-center items-center cursor-pointer border-solid border-2 border-gray-600" 
+  style={`background-image: url(${bgImg});
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat`}>
+  <div class="w-fit bg-slate-800 bg-opacity-70 p-2 rounded-xl">        
+    <p class="text-lg text-center text-white font-semibold">{name}</p>  
+  </div>
+</div>
